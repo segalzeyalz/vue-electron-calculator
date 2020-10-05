ec730a8f97395e3a565ee031b6bd641d13f7553c
eyal segal <eyal.segal@aidoc.com>
2020-10-05 23:43:43 +0300

    infrastructure

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Calculator title="Calculator "/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Calculator from '@/components/Calculator.vue'; // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    Calculator,
+  },
+});
+</script>
